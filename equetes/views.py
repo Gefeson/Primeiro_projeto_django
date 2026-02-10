8e6cb7d53ce643899c1702adbbd787143edb8d4d
Gefeson <geffm4a1@gmail.com>
2026-02-10 14:31:19 -0300

    Atualização na pasta views.py

--- equetes/views.py
+++ equetes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render 
 from django.http import HttpResponse
 from equetes.models import Questao
 
