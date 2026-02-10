cf7b2d30c26e3f5233a64bc91237231a035d1f4a
Gefeson <geffm4a1@gmail.com>
2026-02-10 16:13:00 -0300

    adição do build.sh 4

--- build.sh
+++ build.sh
@@ -1,25 +1,30 @@
-#!/user/bin/env bash
-#Interrompe a execução se algum comando falhar
+#!/usr/bin/env bash
 set -o errexit
 
-#instala as dependencias
+# instala as dependências
 pip install -r requirements.txt
 
-#coleta arquivos estaticos
-python manage.py collectstatic --no-input
+# coleta arquivos estáticos
+python manage.py collectstatic --noinput
 
-#aplica as migrações do banco de dados
+# aplica migrações
 python manage.py migrate
 
-#cria superusuario se não existir
-if ["$SUPERUSER_NAME"]; then
-    python manage.py shell << END
+# cria superusuário se variáveis existirem
+if [ -n "$SUPERUSER_NAME" ]; then
+python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
-if not User.objects.filter(username='$SUPERUSER_NAME').exits():
-    User.objects.creat_superuser('$SUPERUSER_NAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')
-    print("Superuser criado com sucesso!)
+
+if not User.objects.filter(username="$SUPERUSER_NAME").exists():
+    User.objects.create_superuser(
+        "$SUPERUSER_NAME",
+        "$SUPERUSER_EMAIL",
+        "$SUPERUSER_PASSWORD"
+    )
+    print("Superuser criado com sucesso!")
 else:
     print("Superuser já existe!")
-END
+EOF
 fi
