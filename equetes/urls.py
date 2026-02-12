b25aa660237ca4a0351ed1337bcd38cdafba36fc
Gefeson <geffm4a1@gmail.com>
2026-02-12 09:45:54 -0300

    Correção de 'detalhe' para 'detalhes' em views

--- equetes/urls.py
+++ equetes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from equetes import views
+from . import views
 
 app_name = "equetes"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:questao_id>/", views.detalhe, name="detalhes"),
+    path("<int:questao_id>/", views.detalhes, name="detalhes"),
     path("<int:questao_id>/voto/", views.voto, name="voto"),
     path("<int:questao_id>/resultados/", views.resultados, name="resultados"),
 ]
