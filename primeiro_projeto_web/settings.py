a9be0f83bde590c71a674fc8e91487dd43ee9f32
Gefeson <geffm4a1@gmail.com>
2026-02-12 14:37:03 -0300

    Correção na mudanca no settings

--- primeiro_projeto_web/settings.py
+++ primeiro_projeto_web/settings.py
@@ -122,7 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    }
+}
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
