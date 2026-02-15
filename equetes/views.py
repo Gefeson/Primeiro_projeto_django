6f767cb5cdc6ce9f8fdb9af4e87aa848a5c2aeec
Gefeson <geffm4a1@gmail.com>
2026-02-14 22:56:16 -0300

    Teste botao voltar

--- equetes/views.py
+++ equetes/views.py
@@ -1,9 +1,11 @@
 
-from django.shortcuts import get_object_or_404, render 
+from django.utils import timezone
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 from .models import Questao, Alternativa
+from .forms import QuestaoForm, AlternativaForm
 
 
 def index(request):
@@ -34,4 +36,32 @@ def voto(request, questao_id):
 def resultados(request, questao_id):
     questao = get_object_or_404(Questao, pk=questao_id)
     return render(request, 'equetes/resultados.html', {'questao':questao})
-    
+
+def nova_enquete(request):
+    if request.method == "POST":
+        try:
+            form = QuestaoForm(request.POST)
+            if form.is_valid():
+                # salva questao mas não envia ao banco ainda (commit=False)
+                # para podermos definir a data de publicação via código
+                questao = form.save(commit=False)
+                questao.data_pub = timezone.now()
+                questao.save()
+                return redirect('equetes:index')
+        except Exception as e:
+            form.add_error(None,f"Ocorreu um erro inesperado. {e}")
+    else:
+        form = QuestaoForm()
+        
+    return render(request, 'equetes/cadastro.html', {'form': form})
+
+'''def nova_alternativa(request, questao_id):
+    questao = get_object_or_404(Questao, pk= questao_id)
+    if request.method == "POST":
+        try:
+            form = AlternativaForm(request.POST)
+            if form.is_valid():
+                alternativa = form.save(commit=False)
+                alternativa.questao = questao
+                alternativa.save()
+            return redirect('equetes:index')'''
